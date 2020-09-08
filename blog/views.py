1b8ac0d8d3d884ed22e0fa08a9031537e1f70111
NikZprojects <NikZprojects@gmail.com>
2020-09-08 15:42:55 -0400

    edited jinja variables (weren't passing to template before)

--- blog/views.py
+++ blog/views.py
@@ -23,35 +23,35 @@ def all_recipes_redirect():
 def banana_bread_view(request, *args, **kwargs):
     name = "The Best Banana Bread (Two Ways)"
     description = "You can't go wrong with this banana bread recipe."
-    return render(request, "recipes/banana_bread.html", {name:name, description:description})
+    return render(request, "recipes/banana_bread.html", {'name':name, 'description':description})
 def banana_bread_redirect():
     return redirect("/recipes/banana_bread/", *args, permanent=True, **kwargs)
 
 def breakfast_sandwich_view(request, *args, **kwargs):
     name = "Deli-Style Breakfast Sandwich"
     description = "Bagel shop taste from the comfort of your own home."
-    return render(request, "recipes/breakfast_sandwich.html", {name:name, description:description})
+    return render(request, "recipes/breakfast_sandwich.html", {'name':name, 'description':description})
 def breakfast_sandwich_redirect():
     return redirect("/recipes/breakfast_sandwich/", *args, permanent=True, **kwargs)
 
 def cookie_dough_cupcakes_view(request, *args, **kwargs):
     name = "Chocolate Chip Cookie Dough Cupcakes"
     description = "Vanilla cupcakes with a soft, cookie dough center."
-    return render(request, "recipes/cookie_dough_cupcakes.html", {name:name, description:description})
+    return render(request, "recipes/cookie_dough_cupcakes.html", {'name':name, 'description':description})
 def cookie_dough_cupcakes_redirect():
     return redirect("/recipes/cookie_dough_cupcakes/", *args, permanent=True, **kwargs)
 
 def lemon_chicken_view(request, *args, **kwargs):
     name = "Simple & Healthy Lemon Chicken"
     description = "The best 470 calorie dinner you'll have this week."
-    return render(request, "recipes/lemon_chicken.html", {name:name, description:description})
+    return render(request, "recipes/lemon_chicken.html", {'name':name, 'description':description})
 def lemon_chicken_redirect():
     return redirect("/recipes/lemon_chicken/", *args, permanent=True, **kwargs)
 
 def sweet_cream_cold_brew_view(request, *args, **kwargs):
     name = "Vanilla Sweet Cream Cold Brew"
     description = "The closest you can get to Starbucks at home."
-    return render(request, "recipes/sweet_cream_cold_brew.html", {name:name, description:description})
+    return render(request, "recipes/sweet_cream_cold_brew.html", {'name':name, 'description':description})
 def sweet_cream_cold_brew_redirect(request, *args, **kwargs):
     return redirect("/recipes/sweet_cream_cold_brew/", *args, permanent=True, **kwargs)
 
@@ -63,13 +63,13 @@ def experiments_redirect(request, *args, **kwargs):
 def edible_cookie_dough_view(request, *args, **kwargs):
     name = "Experiment #001: Edible Cookie Dough"
     description = "Can baking soda improve the taste of edible cookie dough?"
-    return render(request, "experiments/edible_cookie_dough.html", {name:name, description:description})
+    return render(request, "experiments/edible_cookie_dough.html", {'name':name, 'description':description})
 def edible_cookie_dough_redirect(request, *args, **kwargs):
     return redirect("/experiments/edible_cookie_dough/", *args, permanent=True, **kwargs)
 
 def page_not_found_view(request, *args, **kwargs):
     return render(request, 'errors/404.html', {})
-    
+
 def server_error_view(request, *args, **kwargs):
     return render(request, 'errors/500.html', {})
 
